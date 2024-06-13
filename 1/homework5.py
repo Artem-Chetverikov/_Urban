6dd3a290f45944298602bcb45d06f3ef25fb2a69
Artem Chetverikov <av.chetverikov@tensor.ru>
2024-06-13 22:57:32 +0500

    no message

--- 1/homework5.py
+++ 1/homework5.py
@@ -1,6 +1,6 @@
 immutable_var = (1, 2, 'a', 'b')
 print(immutable_var[0])
-#immutable_var[0] = 3    # Ошибка! Кортеж - неизменяемый тип данных
+immutable_var[0] = 3    # Ошибка! Кортеж - неизменяемый тип данных
 mutable_list = [1, 2, 'a', 'b', 'Modified']
 mutable_list[0] = 'Один'
 mutable_list[4] = 5
