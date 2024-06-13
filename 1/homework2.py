c9dd45bd3dcf43118e2ea91d0f535d9cdd2de170
Artem Chetverikov <av.chetverikov@tensor.ru>
2024-06-13 22:33:19 +0500

    no message

---
+++ 1/homework2.py
@@ -0,0 +1,5 @@
+hw_count = 12
+hours_count = 1.5
+course_name = 'Python'
+orange_count = 15
+print(course_name, ', всего задач:', hw_count, ', затрачено часов:', hours_count, ', среднее время выполнения', hours_count/hw_count , 'часа.')
---
+++ 1/homework3.py
@@ -0,0 +1,8 @@
+name = 'Артем'
+print(name)
+age = 38
+print(age)
+age = age + 2
+print(age)
+is_student = True
+print(is_student)
