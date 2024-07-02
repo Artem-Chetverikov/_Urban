d27bbeb5ebe84296bc6862e79c4e872eb8a5427b
Artem Chetverikov <av.chetverikov@tensor.ru>
2024-07-02 13:21:32 +0500

    *

--- 1/module_3_4.py
+++ 1/module_3_4.py
@@ -3,12 +3,12 @@ def single_root_words(root_word, *other_words):
     for word_i in other_words:
         if root_word.lower() in word_i.lower() and not (word_i in same_words):
             same_words.append(word_i)
-        if word_i.lower() in root_word.lower() and not (root_word in same_words):
+        if word_i.lower() in root_word.lower() and not (word_i in same_words):
             same_words.append(word_i)
     return same_words
 
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
-result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Disable', 'Bagel')
 print(result1)
 print(result2)
