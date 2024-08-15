799fe0096fe240be2816dead09ae0319312a50fe
Artem Chetverikov <av.chetverikov@tensor.ru>
2024-08-15 09:41:58 +0500

    *

--- 1/module_7_2/module_7_2.py
+++ 1/module_7_2/module_7_2.py
@@ -1,12 +1,12 @@
 def custom_write(file_name, strings):
     strings_positions = dict()
     strings = enumerate(strings, 1)
+    file = open(file_name, 'w', encoding='utf-8')
     for num_i, str_i in strings:
-        file = open(file_name, 'a', encoding='utf-8')
         key_str = tuple([num_i, file.tell()])
         file.write(str_i + '\n')
-        file.close()
         strings_positions[key_str] = str_i
+    file.close()
     return strings_positions
 
 
@@ -16,6 +16,7 @@ info = [
     'Because there are 2 languages!',
     'Спасибо!'
 ]
+
 
 result = custom_write('test.txt', info)
 for elem in result.items():
