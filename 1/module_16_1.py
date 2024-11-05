21108579c959aeabdc96cfe2b867029b9de9eb77
Artem Chetverikov <av.chetverikov@tensor.ru>
2024-11-05 22:54:10 +0500

    *

---
+++ 1/module_16_1.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return "Главная страница"
+
+
+@app.get("/user/admin")
+async def root_adm():
+    return "Вы вошли как администратор"
+
+
+@app.get("/user/{user_id}")
+async def root_us(user_id):
+    return f"Вы вошли как пользователь № {user_id}"
+
+
+@app.get("/user")
+async def root_us_param(username, age):
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
