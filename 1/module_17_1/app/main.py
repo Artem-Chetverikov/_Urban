801a1121e5bbbaae0c48006bf409c1face5ec20d
Artem Chetverikov <av.chetverikov@tensor.ru>
2024-11-19 23:03:03 +0500

    *

---
+++ 1/module_17_1/app/main.py
@@ -0,0 +1,19 @@
+import sys
+
+sys.path.insert(0, './1/module_17_1/app')
+
+from fastapi import FastAPI
+from routers import task, user
+
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome():
+    return {"message": "Welcome to Taskmanager"}
+
+
+app.include_router(task.router_task)
+app.include_router(user.router_user)
+
+# python -m uvicorn 1.module_17_1.app.main:app
